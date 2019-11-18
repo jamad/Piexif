49233a15bbaff50cf71a9a352e159c7d5851cf2d
jamad <jamad2001@gmail.com>
2019-11-18 15:21:48 +0200

    Create sample.py
    sample usage by myself

---
+++ sample.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import piexif
+
+filename=r"C:\\Users\\jamad\\Desktop\\IMG-4199.JPG"  
+#filename=r"E:\\naue\\naue\\0087-01.jpg"  
+
+
+D = piexif.load(filename)
+for k in D:
+    if k=='thumbnail':continue
+    if k=='Exif':E=D[k]
+    print(k,D[k])
+
+for k in E:
+    print(k)
+#print(D)
+#if piexif.ImageIFD.Orientation in D["0th"]:print("Orientation is ", D["0th"][piexif.ImageIFD.Orientation])
+#if piexif.ExifIFD.Gamma in D["Exif"]:print("Gamma is ", D["Exif"][piexif.ExifIFD.Gamma])
