31932955a9192587b3e4f09864129cce74d379dd
jamad <jamad2001@gmail.com>
2019-11-18 15:27:24 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,15 +3,13 @@ import piexif
 
 filename=r"C:\\Users\\jamad\\Desktop\\IMG-4199.JPG"  
 
-
 D = piexif.load(filename)
 for k in D:
     if k=='thumbnail':continue
     if k=='Exif':E=D[k]
     print(k,D[k])
 
-for k in E:
-    print(k)
+for k in E:print(k)
 #print(D)
 #if piexif.ImageIFD.Orientation in D["0th"]:print("Orientation is ", D["0th"][piexif.ImageIFD.Orientation])
 #if piexif.ExifIFD.Gamma in D["Exif"]:print("Gamma is ", D["Exif"][piexif.ExifIFD.Gamma])
